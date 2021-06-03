1affed363617defdc67a553b36db09976b9400c3
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-06-03 09:35:39 -0400

    refactor form component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import Header from '../header/Header';
-import { HeroSection, HeroContent, Form, Button } from './heroStyles';
-import { BsChevronRight } from 'react-icons/bs';
-import { useForm } from 'react-hook-form';
+import { HeroSection, HeroContent } from './heroStyles';
+import Form from '../form/Form';
 
 const Hero = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
-
-    const handleOnSubmit = (data) => {
-        console.log(data);
-    };
     return (
         <HeroSection>
             <Header />
@@ -21,37 +11,8 @@ const Hero = () => {
                 <div>
                     <h1>Unlimited movies, TV shows, and more.</h1>
                     <p>Watch anywhere. Cancel anytime.</p>
-                    <p>
-                        Ready to watch? Enter your email to create or restart
-                        your membership.
-                    </p>
-                    <Form onSubmit={handleSubmit(handleOnSubmit)}>
-                        <div>
-                            <input
-                                type='text'
-                                {...register('email', {
-                                    required: 'Email is required!',
-                                    pattern: {
-                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                        message:
-                                            'Please enter a valid email address',
-                                    },
-                                })}
-                                id='email'
-                                placeholder='Email address'
-                            />
-                            {errors.email && (
-                                <span>{errors.email.message}</span>
-                            )}
-                        </div>
-                        <Button type='submit'>
-                            Get Started
-                            <span>
-                                <BsChevronRight />
-                            </span>
-                        </Button>
-                    </Form>
                 </div>
+                <Form />
             </HeroContent>
         </HeroSection>
     );
