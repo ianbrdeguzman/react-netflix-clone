5e298b520fbffb031eb108a52d383e8761394bf1
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-06-02 21:27:34 -0400

    added footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,21 +3,22 @@ import Hero from '../../components/hero/Hero';
 import Feature from '../../components/feature/Feature';
 import { Border } from './homeStyles';
 import { data } from '../../components/feature/featureData';
+import Footer from '../../components/footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <Border />
-            {data.map((feature) => {
+            {data.map((feature, i) => {
                 return (
-                    <>
+                    <div key={i}>
                         <Feature key={feature.id} {...feature} />
                         <Border />
-                    </>
+                    </div>
                 );
             })}
-            <Border />
+            <Footer />
         </div>
     );
 };
