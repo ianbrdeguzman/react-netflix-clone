2f8e9b1b5f60b960af54613a747fd0c3a0c197fc
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-06-03 11:30:08 -0400

    fixed feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -6,8 +6,48 @@ import {
     ImageContainer,
     VideoContainer,
 } from './featureStyles';
+import styled from 'styled-components';
 
-const Feature = ({ title, text, image, direction, video, videoSize }) => {
+const Boxshot = styled.div`
+    position: absolute;
+    bottom: 10%;
+    left: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 80%;
+    height: 25%;
+    padding: 1rem;
+    border: 2px solid #222222;
+    border-radius: 1rem;
+    background-color: #000000;
+    div:nth-child(1) {
+        flex: 0.2;
+    }
+    div:nth-child(2) {
+        flex: 0.8;
+        padding: 0 1rem;
+        p:first-child {
+            font-weight: bold;
+        }
+        p:last-child {
+            color: #0071eb;
+        }
+    }
+    div:nth-child(3) {
+        flex: 0.2;
+    }
+`;
+
+const Feature = ({
+    title,
+    text,
+    image,
+    direction,
+    video,
+    videoSize,
+    boxshot,
+}) => {
     return (
         <StyledFeature>
             <FeatureContent direction={direction}>
@@ -24,6 +64,20 @@ const Feature = ({ title, text, image, direction, video, videoSize }) => {
                             </video>
                         )}
                     </VideoContainer>
+                    {boxshot && (
+                        <Boxshot>
+                            <div>
+                                <img src={boxshot.image} alt='boxshot' />
+                            </div>
+                            <div>
+                                <p>Stranger Things</p>
+                                <p>Downloading...</p>
+                            </div>
+                            <div>
+                                <img src={boxshot.gif} alt='downloading-gif' />
+                            </div>
+                        </Boxshot>
+                    )}
                 </ImageContainer>
             </FeatureContent>
         </StyledFeature>
